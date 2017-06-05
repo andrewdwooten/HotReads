05aa76189c418498368975e5a958cb94b80f3f9f
Andy Wooten <andrewdwooten@icloud.com>
2017-06-05 16:32:15 -0600

    Add top_read class method to read model Same returns top hit for the past 24 hours.

--- app/models/read.rb
+++ app/models/read.rb
@@ -6,4 +6,11 @@ class Read < ApplicationRecord
     .limit(10)
     .group(:url).count
   end
+
+  def self.top_read
+    where(created_at: (Time.now.midnight - 1.day)..Time.now)
+    .order('count(*) DESC')
+    .limit(10)
+    .group(:url).count.first
+  end
 end
