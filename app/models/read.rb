6d81a5ec7549a4c6059f16a470c95de49d4fafce
Andy Wooten <andrewdwooten@icloud.com>
2017-06-05 16:30:39 -0600

    Add class hot_reads method
    Same returns url and count of top url hits for the day.

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,2 +1,9 @@
 class Read < ApplicationRecord
+
+  def self.hot_reads
+    where(created_at: (Time.now.midnight - 1.day)..Time.now)
+    .order('count(*) DESC')
+    .limit(10)
+    .group(:url).count
+  end
 end
